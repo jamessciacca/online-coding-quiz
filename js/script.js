4026ca4980ec488d6b7ad746e5c6dfa4102cbc5c
jamessciacca <noing2003@gmail.com>
2023-03-27 23:16:20 -0400

    clocking out for the night

--- js/script.js
+++ js/script.js
@@ -1,21 +1,29 @@
 //variables and querySelector elements
-var startBtn = document.querySelector('#startBtn');
+var startBtn = document.querySelector('#startBtn')
+var startSection = document.querySelector('#startSection');
 var questionEl = document.querySelector('#question');
 var choicesEl = document.querySelector('#choices')
 var timerEl = document.querySelector('#timer')
 var questionSection = document.querySelector('#questionSection')
 var endSection = document.querySelector('#endSection')
+var correctOrWrong = document.querySelector('#correctorwrong')
+var timerDiv = document.querySelector('.timerDiv')
+var scorecardDiv = document.querySelector('#scorecard')
+var scoreEl = document.querySelector('#score')
+var finalScore = document.querySelector('#finalscore')
 var questionUserIsOn = 0;
 var timer;
 var seconds = 59;
 var interval;
+var score = -1;
 
 //function to hide welcome page and show timer and question
 function startQuiz() {
-    startBtn.classList.add('hide');
+    startSection.classList.add('hide');
 
     showQuestion()
     onScreenTimer()
+    keepScore()
 }
 
 //function that creates both the title and choices to screen
@@ -38,19 +46,20 @@ function checkAnswer(event) {
 
     if (userChoice === questions[questionUserIsOn].answer) {
         choicesEl.innerHTML = '';
-        console.log('correct')
+        //prints correct to screen
+        correctOrWrong.innerText = 'Correct!'
         questionUserIsOn = questionUserIsOn + 1;
         questionsLeft(questionUserIsOn);
+        //if user gets the question right I call keepScore
+        keepScore();
     } else {
-            choicesEl.innerHTML = '';
-            console.log('wrong')
-            questionUserIsOn = questionUserIsOn + 1;
-            questionsLeft(questionUserIsOn);
-            timeDeduction();
-        
-        //deduct 10 sec from timer
-
-        console.log(questionUserIsOn)
+        choicesEl.innerHTML = '';
+        //prints wrong to screen
+        correctOrWrong.innerText = 'Wrong!'
+        questionUserIsOn = questionUserIsOn + 1;
+        questionsLeft(questionUserIsOn);
+        //deducts 10 sec from timer
+        timeDeduction();
     }
 }
 
@@ -63,19 +72,35 @@ function questionsLeft (questionUserIsOn){
     }
 }
 
-//function that sends user to the end screen
-function endQuiz (){
+//a function that ads a point when the user answers the question right, also displays the current score to the screen!
+function keepScore(){
+    scorecardDiv.classList.remove('hide')
+    score = score + 1;
+    scoreEl.innerHTML = score;
+}
 
+
+//function that sends user to the end screen
+function endQuiz(){
     questionSection.classList.add('hide')
-    endSection.innerHTML = 'Quiz Over!';
+    endSection.classList.remove('hide')
+    //prints final score
+    finalScore.innerHTML = score;
 }
 
 //function that displays the timer when start button is hit! 
 function onScreenTimer(){
-    timerEl.classList.remove('hide')
+    timerDiv.classList.remove('hide')
     timer = setInterval(() =>{
-        timerEl.innerHTML = 'Time Remaining - 00:'+ seconds + ' seconds!';
+        timerEl.innerHTML = seconds;
         seconds --;
+
+        //if user runs out of time I want to stop timer and send user to end screen!
+        if(seconds <= 0){
+            clearInterval(timer);
+            endQuiz();
+        }
+
     }, 1000)//1 second
 }
 
